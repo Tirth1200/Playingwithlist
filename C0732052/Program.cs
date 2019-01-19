0091a5a8aa7b754073224873badccb7796488ddb
visualstudio <visualstudio@122-PC16>
2019-01-18 20:40:55 -0500

    Added a code

--- C0732052/Program.cs
+++ C0732052/Program.cs
@@ -10,6 +10,24 @@ namespace C0732052
     {
         static void Main(string[] args)
         {
+            StudentInformation Mary = new StudentInformation("Marry", 3.9, 20);
+            StudentInformation John = new StudentInformation("John", 2.0, 10);
+            StudentInformation Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation Steven = new StudentInformation("Steven", 2.9, 10);
+            StudentInformation Cindy = new StudentInformation("Cindy", 3.8, 20);
         }
     }
+    class StudentInformation
+    {
+        public string Name;
+        public double GPA;
+        public int Attendance;
+        public StudentInformation(string aName,double aGPA,int aAttendance)
+        {
+            Name = aName;
+            GPA = aGPA;
+            Attendance = aAttendance;
+        }
+    }
+
 }
